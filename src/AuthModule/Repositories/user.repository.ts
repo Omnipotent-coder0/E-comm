e8a8553d977247d10f99bf57b4a2e7d580c4656d
Nilesh Gautam <nileshgautam13092001+git@gmail.com>
2024-11-30 14:52:45 +0530

    Added Initial Boiler codes

---
+++ src/AuthModule/Repositories/user.repository.ts
@@ -0,0 +1,20 @@
+import { promises } from "dns";
+import { IUserInput, User, UserDocument } from "../../shared/modals/user.modal";
+import { BadRequestError } from "../../shared/utils/apiError";
+
+export class UserRepository{
+    constructor(){
+
+    }
+    public getUserByEmail = async(email: string) : Promise<UserDocument>=>{
+        const findUser = await User.findOne({email});
+        console.log(findUser);
+        if(!findUser) throw new BadRequestError("Invalid Credentials !!");
+        return findUser;
+    }
+    public createUser = async(onBoardUserDto: IUserInput) : Promise<UserDocument>=>{
+        const newUser = new User(onBoardUserDto);
+        await newUser.save();
+        return newUser;
+    }
+}
